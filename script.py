2a469b1bf5542afd6a6d7fe450b095fbfca781ad
Rhys Mallia <Rhys Mallia>
2022-04-13 08:11:26 +0000

    added script.py

---
+++ script.py
@@ -0,0 +1,42 @@
+# TO DO
+#
+#
+#
+#
+
+# Module imports
+import sys, json, subprocess
+
+#constants
+
+
+def main():
+    #Either take user input, or begin scanning CSV
+    # userInput()
+    # scanCSV()
+
+    # For loop, set up varibles to send to 
+    
+def userInput()
+{
+    input = ""
+    count = 0
+
+    while(input != "q"):
+        count += 1
+        hostname = "CE"
+        bandwidth = ""
+
+        # take bandwidth
+        while(bandwidth == ""):
+            print("Please enter your bandwidth: ")
+            hostname = input().strip().lower
+
+        while
+}
+
+
+
+
+if __name__ == "__main__":
+    main()
