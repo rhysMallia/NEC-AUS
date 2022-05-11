da607975b6fb2b8d7e885248e19ec3a2fce5bf57
3734303 <s3734303@student.rmit.edu.au>
2022-05-11 23:40:23 +1000

    now with benchmark

--- script.py
+++ script.py
@@ -13,12 +13,15 @@
 #   TOTAL TIME SAVED
 
 # Module imports
+import csv
 import sys, json, subprocess
 import pandas as pd
 import hashlib as hash
 import datetime
+import statisticS
+import time
 
-#constants
+# constants
 defaultFileName = "config"
 extension = ".csv"
 defaultHost = "CE"
@@ -30,21 +33,24 @@ vars = "--extra-vars"
 variableDict = {}
 variableArray = []
 folder = ""
+results = []
 
-def main(): 
+
+def main():
     fileInput()
     generateFolder()
     executeScript()
-    
+
+
 # Checks if the file is present, if not asks for user imput
 def fileInput():
     # read default file    
     filename = defaultFileName + extension
     check = False
     while not check:
-        global variableDict 
-        variableDict = pd.read_csv(filename, header=None, skiprows=1, usecols=range(1,3))
-        #print(variableDict)
+        global variableDict
+        variableDict = pd.read_csv(filename, header=None, skiprows=1, usecols=range(1, 3))
+        # print(variableDict)
         if variableDict.empty:
             # if the dict is empty, try to locate another file
             print("Unable to locate default config file.")
@@ -54,18 +60,20 @@ def fileInput():
         else:
             # if the dict is full, we can exit the loop
             # variableDict = variableDict.to_dict()
-            #print(variableDict)
+            # print(variableDict)
             check = True
+
 
 # Generates a tailored folder to paste the generated CSV files
 def generateFolder():
     global folder
     # get the date time 
     x = datetime.datetime.now()
-    
+
     # Convert into usable format and set as the global variable
     folder = x.strftime("%H%M%d%B%Y")
-    
+
+
 # Generates the variables and sends them to the ansible playbook
 def executeScript():
     global variableDict
@@ -81,42 +89,43 @@ def executeScript():
     ipAddr = 1
     # The length of the dict show us how many rows
     rows = len(variableDict)
-    
+
     # Iterate over every row and create the variables before launching the playbook
     for rows in range(0, rows):
+        start = time.perf_counter()
         # Router description
         desc = variableDict[1][rows]
         # Bandwidth of connection
         bandwidth = variableDict[2][rows]
         # Dict to turn into JSON
         variableHolder = {}
-        #print(desc)
-        #print(bandwidth)
-        
+        # print(desc)
+        # print(bandwidth)
+
         # Hostname
         hostname = createHost(count)
         # Shape Averages THIS ISN'T WORKING 100%
-        shapeAvg1 = int((bandwidth * 10**5) * 0.97)
-        shapeAvg2 = int((bandwidth * 10**3) * 0.97)
+        shapeAvg1 = int((bandwidth * 10 ** 5) * 0.97)
+        shapeAvg2 = int((bandwidth * 10 ** 3) * 0.97)
         # Policy Maps
         policy = str(bandwidth) + "MB"
         # IP address
-        ipAddress = ip + str(ipRange) + "." + str(ipAddr) 
+        ipAddress = ip + str(ipRange) + "." + str(ipAddr)
         # tunnel bandwidth
-        tunnelBandwidth = int((bandwidth * 10**3))
+        tunnelBandwidth = int((bandwidth * 10 ** 3))
         # MD5 Hash
         temp = (hostname + desc + str(ipAddress) + str(bandwidth))
         digestKey = hash.md5(temp.encode('utf-8')).hexdigest()
-        
+
         # Testing prints
-        #print(bandwidth)
-        #print(desc)
-        #print(shapeAvg1)
-        #print(shapeAvg2)
-        #print(policy)
-        #print(ipAddress)
-        #print(digestKey)
-        
+        # print(bandwidth)
+        # print(desc)
+        # print(shapeAvg1)
+        # print(shapeAvg2)
+        # print(policy)
+        # print(ipAddress)
+        # print(digestKey)
+
         # Add to Dict object
         variableHolder['hostname'] = str(hostname)
         variableHolder['bandwidth'] = str(bandwidth)
@@ -128,20 +137,19 @@ def executeScript():
         variableHolder['digestKey'] = str(digestKey)
         variableHolder['folder'] = str(folder)
 
-        print(variableHolder)
+        # print(variableHolder)
         # Convert dict object to json
         variableHolder = json.dumps(variableHolder)
-
 
         # Send object to run ansible command
         result = subprocess.run(
             [ansible, playbook, vars, variableHolder],
-            capture_output = True, text = True
+            capture_output=True, text=True
         )
 
         # print the stdout and error to the console
-        print("stdout: " + result.stdout)
-        print("stderr: " + result.stderr)
+        # print("stdout: " + result.stdout)
+        # print("stderr: " + result.stderr)
 
         # Iterate nessicary counters
         if ipAddr >= 250:
@@ -150,15 +158,36 @@ def executeScript():
         else:
             ipAddr += 1
         count += 1
+        print(benchmark(hostname, start))
+    print(benchmark_output(count))
+
 
 # This function will ensure that the hostname will always be 3 digits with leading zeros
 def createHost(count):
     count = str(count)
     hostname = defaultHost
 
-    while(len(count) < 3):
+    while (len(count) < 3):
         count = "0" + count
     return hostname + count
+
+
+def benchmark(hostname, start):
+    timelapse = time.perf_counter() - start
+    result = [hostname, timelapse]
+    results.append(result)
+    return f'config {hostname} generated, timelapse: {timelapse:.4f} seconds'
+
+
+def benchmark_output(count):
+    with open('benchmark.csv', 'w', newline='') as file:
+        write_head = csv.writer(file, delimiter=',')
+        write_head.writerows(results)
+    total = 0
+    for x in results:
+        total = total + x[1]
+
+    return f'average time:{total/count:.4f}'
 
 
 if __name__ == "__main__":
