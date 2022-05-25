b0a34dcda8eaf9b394810b3a7aceadea4a3a3bc3
Rhys <34205840+rhysMallia@users.noreply.github.com>
2022-05-25 23:12:25 +1000

    added user input

--- generateConfig.py
+++ generateConfig.py
@@ -1,12 +1,20 @@
 import csv
 import random
+import sys
 
-devices = 502
+devices = 0
 counter = 1
 header = ['No', ' ', 'bandwidth (MB)']
 bandwidth = [10, 20, 50, 100, 200, 300, 500, 1000]
+
 def randomBandwidth():
     return bandwidth[random.randrange(0, len(bandwidth), 1)]
+
+# check for extra vars
+if len(sys.argv) > 1:
+    devices = int(sys.argv[0])
+else:
+    devices = 502
 
 # open the file ( with keyword means you don't have to close)
 with open('config.csv', 'w', encoding='UTF8', newline='') as f:
