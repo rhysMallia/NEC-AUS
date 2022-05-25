ab82165d27d74beefbaf5401e3c7b59f797ff232
Rhys <34205840+rhysMallia@users.noreply.github.com>
2022-05-25 23:23:54 +1000

    fixed indentation

--- generateConfig.py
+++ generateConfig.py
@@ -2,7 +2,7 @@ import csv
 import random
 import sys
 
-devices = 502
+devices = 0
 counter = 1
 header = ['No', ' ', 'bandwidth (MB)']
 bandwidth = [10, 20, 50, 100, 200, 300, 500, 1000]
@@ -16,24 +16,26 @@ def generateCSV():
     global counter
     global header
 # check for extra vars
-    if len(sys.argv) > 1:
-        devices = sys.argv[0]
+    if len(sys.argv) > 2:
+        devices = sys.argv[1]
+    else:
+        devices = 502
 
-        # open the file ( with keyword means you don't have to close)
-        with open('config.csv', 'w', encoding='UTF8', newline='') as f:
-            # inform user
-            print(str(devices) + " devices will be generated...")
+    # open the file ( with keyword means you don't have to close)
+    with open('config.csv', 'w', encoding='UTF8', newline='') as f:
+        # inform user
+        print(str(devices) + " devices will be generated...")
 
-            # Create the writer 
-            writer = csv.writer(f)
+        # Create the writer 
+        writer = csv.writer(f)
 
-            # Write the header
-            writer.writerow(header)
+        # Write the header
+        writer.writerow(header)
 
-            while(counter != devices):
-                data = [counter, '', randomBandwidth()]
-                writer.writerow(data)
-                counter += 1
+        while(counter != devices):
+            data = [counter, '', randomBandwidth()]
+            writer.writerow(data)
+            counter += 1
 
 if __name__ == '__main__':
     generateCSV()
