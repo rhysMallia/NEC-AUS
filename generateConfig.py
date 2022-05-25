8c2ddb45c1d91a521e632569afd49753de83ae42
Rhys <34205840+rhysMallia@users.noreply.github.com>
2022-05-25 23:22:56 +1000

    added global variables

--- generateConfig.py
+++ generateConfig.py
@@ -8,9 +8,13 @@ header = ['No', ' ', 'bandwidth (MB)']
 bandwidth = [10, 20, 50, 100, 200, 300, 500, 1000]
 
 def randomBandwidth():
+    global bandwidth
     return bandwidth[random.randrange(0, len(bandwidth), 1)]
 
 def generateCSV():
+    global devices
+    global counter
+    global header
 # check for extra vars
     if len(sys.argv) > 1:
         devices = sys.argv[0]
